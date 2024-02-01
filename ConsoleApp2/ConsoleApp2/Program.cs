99c057934f7078a7010afb4b33b01524a250f18a
Nikita Pomelov <krt3298@gmail.com>
2024-02-01 10:04:29 +0300

    .

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double s = 0;
+            int k = 1;
+            //int k = 15;
+
+            while (k <= 15)
+            {
+                s += k * x * Math.Sin(x);
+                k++;
+            }
+
+            //do
+            //{
+            //    s += k * x * Math.Sin(x);
+            //}
+            //while (++k <= 15);
+
+            //while (k >= 1)
+            //{
+            //    s += k * x * Math.Sin(x);
+            //    k--;
+            //}
+
+            //do
+            //{
+            //    s += k * x * Math.Sin(x);
+            //}
+            //while (k-- >= 1);
+
+            Console.WriteLine(s);
+        }
+    }
+}
